af4906d0aff0b8cd96bbb2c765c2033314bf6d1d
Hodsha <88711400+Hodsha@users.noreply.github.com>
2024-01-16 13:21:03 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,54 @@
-import Image from 'next/image'
+import img from "./../../public/quiz.jpg";
 
 export default function Home() {
   return (
-   <main>
-    <h1>Welcome</h1>
-   </main>
+    <div className="relative h-full overflow-hidden">
+      <div
+        className="absolute inset-0 bg-cover bg-center sm:bg-right md:bg-right lg:bg-left xl:bg-center"
+        style={{
+          backgroundImage: 'url("/quiz.jpg")',
+          height: '100%',
+          width: '100%',
+          position: 'absolute',
+          backgroundSize: 'cover',
+          backgroundPosition: 'left',
+          backgroundPositionY: '-50px', // Beispiel: Bild um 50 Pixel nach oben verschieben
+        }}
+      >
+        <h1></h1>
+        </div>
+      <div className="sm:hidden">
+   
+        <div
+          className="absolute inset-0 bg-cover bg-center"
+          style={{
+            backgroundImage: 'url("/quiz-small.jpg")',
+            height: '100%',
+            width: '100%',
+            position: 'absolute',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundPositionY: '-50px',
+          }}
+        >
+          <h1>was geht ab</h1>
+        </div>
+      </div>
+      <div className="hidden sm:block md:hidden">
+        {/* Dieser Abschnitt wird bei Bildschirmen größer als `sm` und kleiner als `md` angezeigt */}
+        <div
+          className="absolute inset-0 bg-cover bg-center"
+          style={{
+            backgroundImage: 'url("/quiz-medium.jpg")',
+            height: '100%',
+            width: '100%',
+            position: 'absolute',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundPositionY: '-50px',
+          }}
+        />
+      </div>
+    </div>
   )
 }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,44 +5,27 @@ import { MdLightMode, MdDarkMode } from "react-icons/md";
 export default function Nav() {
     const { userId } = auth();
     console.log(userId);
-    // const [isDarkMode, setDarkMode] = useState(false);
 
-    // const switchTheme = () => {
-    //     if (isDarkMode == false) {
-    //         setDarkMode(true);
-    //     }
-    //     setDarkMode(false);
-    // }
     return (
-        <>
-            <nav className='bg-blue-700 py-4 p-6 flex items-center justify-between'>
-                <div className='flex items-center'>
-                    <Link href="/">
-                        <div className="text-lg-uppercase font-bold text-white">
-                            Quiz App
-                        </div>
-                    </Link>
-                </div>
-                <div className="text-white flex items-center">
-                    {!userId && (
-                        <>
-                            {/* {isDarkMode ? (
-
-                                <MdLightMode size={20} className='mr-5' />
-                            ) : (
-                                <MdDarkMode size={20} className='mr-5' />
-                            )} */}
-
-                            <Link href='sign-in' className='text-gray-300 hover:text-white mr-4'> Sign In</Link>
-                            <Link href='sign-up' className='text-gray-300 hover:text-white mr-4'> Sign Up</Link>
-                        </>
-                    )}
-                    <div className="ml-auto">
-                        <UserButton afterSignOutUrl='/' />
+        <nav className='bg-slate-950 py-4 p-6 flex items-center justify-between'>
+            <div className='flex items-center'>
+                <Link href="/">
+                    <div className="text-lg-uppercase font-bold text-white">
+                        Quiz App
                     </div>
-
+                </Link>
+            </div>
+            <div className="text-white flex items-center">
+                {!userId && (
+                    <>
+                        <Link href='sign-in' className='text-gray-300 hover:text-white mr-4'> LogIN</Link>
+                        <Link href='sign-up' className='text-gray-300 hover:text-white mr-4'> Sign Up</Link>
+                    </>
+                )}
+                <div className="ml-auto">
+                    <UserButton afterSignOutUrl='/' />
                 </div>
-            </nav>
-        </>
+            </div>
+        </nav>
     )
 }
